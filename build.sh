1a4e74ca6f1da42839d6996999953c615db818b0
Francois Visconte <f.visconte@gmail.com>
2013-10-09 10:33:42 +0200

    Add a build bash script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -xe
+subprojects=bloom_filter
+
+dependencies="github.com/spaolacci/murmur3 \
+              github.com/willf/bitset"
+
+for dep in $dependencies; do
+  echo "Getting $dep..."
+  go get $dep
+done
+
+for subproj in $subprojects; do
+  pushd $subproj > /dev/null
+  echo "Building and testing $subproj..."
+  go build
+  go test
+  popd > /dev/null
+done
